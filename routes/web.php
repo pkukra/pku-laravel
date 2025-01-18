e9b5f06558d19784d75f565f42e866b63ef19e3a
iqbal <emixbal@gmail.com>
2025-01-18 16:51:29 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/studentsdashboard', [StudentController::class, 'index'])->name('studentsdashboard.index');
+    Route::post('/addStudent', [StudentController::class, 'store'])->name('addStudent.store');
+    Route::patch('/updateStudent/{id}', [StudentController::class, 'update'])->name('updateStudent.update');
+    Route::delete('/deleteStudent/{id}', [StudentController::class, 'destroy'])->name('deleteStudent.destroy');
+});
+
+require __DIR__.'/auth.php';
