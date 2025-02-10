501c67a62c0509921468eebb7c72f0e58c54dfad
iqbal <emixbal@gmail.com>
2025-02-10 17:00:20 +0700

    pish diagnosa dari profile

--- resources/js/Pages/RM/PasienRujukan/PasienRujukansDetail.jsx
+++ resources/js/Pages/RM/PasienRujukan/PasienRujukansDetail.jsx
@@ -3,17 +3,16 @@ import { Head } from "@inertiajs/react";
 import axios from "axios"; // Import axios untuk mengambil data
 import { Modal } from "antd"; // Import Modal
 import Button from "@/Components/Button";
-
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import PasienRujukansDetailProfile from "./PasienRujukansDetailProfile";
-import DiagnosaAddBtn from "./DiagnosaAddBtn";
+import DiagnosaList from "./PasienRujukansDetailDiagnosaList";
 
 export default function PasienRujukansDetail({ auth, pasien }) {
     const [diagnosa, setDiagnosa] = useState([]); // State untuk menyimpan data diagnosa
     const [loadingFetchDiagnosa, setLoadingFetchDiagnosa] = useState(true); // Loading state
     const [deleteDiagnosaId, setDeleteDiagnosaId] = useState(null); // Track which diagnosa is being deleted
-    const [selectedDiagnosa, setSelectedDiagnosa] = useState([]);
-    const [isModalHapusDiagnosaOpen, setIsModalHapusDianosaOpen] = useState(false); // Modal visibility
+    const [selectedDiagnosa, setSelectedDiagnosa] = useState([]); // untuk disable diagnosa terpiluh, agar saat menampilkan list diagnosa tidak terpilih 2 kali
+    const [isModalHapusDiagnosaOpen, setIsModalHapusDiagnosaOpen] = useState(false); // Modal visibility
     const [currentDiagnosa, setCurrentDiagnosa] = useState(null); // Track current diagnosa for deletion
 
     // Memanggil endpoint untuk mendapatkan data diagnosa
@@ -56,26 +55,26 @@ export default function PasienRujukansDetail({ auth, pasien }) {
                 setSelectedDiagnosa((prevSelectedDiagnosa) =>
                     prevSelectedDiagnosa.filter((item) => item !== kode)
                 );
-                setDeleteDiagnosaId(null); // Reset deleteDiagnosaId after deletion
                 fetchDiagnosa(); // Memanggil ulang untuk mendapatkan data diagnosa terbaru
-                setIsModalHapusDianosaOpen(false); // Close the modal after deletion
+                setIsModalHapusDiagnosaOpen(false); // Close the modal after deletion
+                setDeleteDiagnosaId(null); // Reset deleteDiagnosaId after deletion
             })
             .catch((error) => {
                 console.error("Error fetching diagnosa data:", error);
                 setDeleteDiagnosaId(null); // Reset deleteDiagnosaId if error occurs
-                setIsModalHapusDianosaOpen(false); // Close the modal if an error occurs
+                setIsModalHapusDiagnosaOpen(false); // Close the modal if an error occurs
             });
     };
 
     // Function to show the modal with the diagnosa info for deletion
     const showDeleteConfirm = (item) => {
         setCurrentDiagnosa(item); // Set the current diagnosa to be deleted
-        setIsModalHapusDianosaOpen(true); // Show the modal
+        setIsModalHapusDiagnosaOpen(true); // Show the modal
     };
 
     // Function to handle cancel (closing the modal)
     const handleCancel = () => {
-        setIsModalHapusDianosaOpen(false); // Close the modal
+        setIsModalHapusDiagnosaOpen(false); // Close the modal
     };
 
     return (
@@ -91,88 +90,16 @@ export default function PasienRujukansDetail({ auth, pasien }) {
             <PasienRujukansDetailProfile pasien={pasien} />
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
-                <div className="diagnosa-list">
-                    <div className="card bg-base-100">
-                        <div className="card-body">
-                            {loadingFetchDiagnosa ? (
-                                <>
-                                    <div className="skeleton h-4 w-full"></div>
-                                    <div className="skeleton h-4 w-full"></div>
-                                    <div className="skeleton h-4 w-full"></div>
-                                    <div className="skeleton h-4 w-full"></div>
-                                    <div className="skeleton h-4 w-full"></div>
-                                </>
-                            ) : (
-                                <>
-                                    <div className="grid grid-cols-5 gap-5">
-                                        <div className="col-span-4">
-                                            <strong>Diagnosa</strong>
-                                        </div>
-                                        <div className="col-span-1">
-                                            <DiagnosaAddBtn
-                                                className="float-end"
-                                                pasien={pasien}
-                                                refreshDiagnosa={fetchDiagnosa}
-                                                selectedDiagnosa={
-                                                    selectedDiagnosa
-                                                }
-                                                setSelectedDiagnosa={
-                                                    setSelectedDiagnosa
-                                                }
-                                            />
-                                        </div>
-                                    </div>
-                                    <table
-                                        className="table table-xs"
-                                        style={{ width: "100%" }}
-                                    >
-                                        <thead>
-                                            <tr>
-                                                <th style={{ width: "5%" }}>
-                                                    NO
-                                                </th>
-                                                <th style={{ width: "10%" }}>
-                                                    Kode
-                                                </th>
-                                                <th style={{ width: "72%" }}>
-                                                    Penyakit
-                                                </th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {diagnosa.map((item, index) => (
-                                                <tr key={index}>
-                                                    <td>{index + 1}</td>
-                                                    <td>
-                                                        {item.MRPKD_PENYAKIT}
-                                                    </td>
-                                                    <td>{item.PENYAKIT}</td>
-                                                    <td>
-                                                        <Button
-                                                            loading={
-                                                                deleteDiagnosaId ===
-                                                                item.ID
-                                                            }
-                                                            className="btn btn-xs btn-outline btn-error"
-                                                            onClick={() =>
-                                                                showDeleteConfirm(
-                                                                    item
-                                                                )
-                                                            }
-                                                        >
-                                                            hapus
-                                                        </Button>
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                </div>
+                <DiagnosaList
+                    pasien={pasien}
+                    diagnosa={diagnosa}
+                    loadingFetchDiagnosa={loadingFetchDiagnosa}
+                    deleteDiagnosaId={deleteDiagnosaId}
+                    showDeleteConfirm={showDeleteConfirm}
+                    selectedDiagnosa={selectedDiagnosa}
+                    setSelectedDiagnosa={setSelectedDiagnosa}
+                    fetchDiagnosa={fetchDiagnosa}
+                />
             </div>
 
             {/* Modal for Confirming Deletion */}
---
+++ resources/js/Pages/RM/PasienRujukan/PasienRujukansDetailDiagnosaList.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import DiagnosaAddBtn from "./DiagnosaAddBtn";
+import Button from "@/Components/Button";
+
+export default function Index({
+    diagnosa,
+    loadingFetchDiagnosa,
+    deleteDiagnosaId,
+    showDeleteConfirm,
+    selectedDiagnosa,
+    setSelectedDiagnosa,
+    fetchDiagnosa,
+    pasien,
+}) {
+    return (
+        <div className="diagnosa-list">
+            <div className="card bg-base-100">
+                <div className="card-body">
+                    {loadingFetchDiagnosa ? (
+                        <>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="grid grid-cols-5 gap-5">
+                                <div className="col-span-4">
+                                    <strong>Diagnosa</strong>
+                                </div>
+                                <div className="col-span-1">
+                                    <DiagnosaAddBtn
+                                        pasien={pasien}
+                                        className="float-end"
+                                        refreshDiagnosa={fetchDiagnosa}
+                                        selectedDiagnosa={selectedDiagnosa}
+                                        setSelectedDiagnosa={
+                                            setSelectedDiagnosa
+                                        }
+                                    />
+                                </div>
+                            </div>
+                            <table
+                                className="table table-xs"
+                                style={{ width: "100%" }}
+                            >
+                                <thead>
+                                    <tr>
+                                        <th style={{ width: "5%" }}>NO</th>
+                                        <th style={{ width: "10%" }}>Kode</th>
+                                        <th style={{ width: "72%" }}>
+                                            Penyakit
+                                        </th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {diagnosa.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.MRPKD_PENYAKIT}</td>
+                                            <td>{item.PENYAKIT}</td>
+                                            <td>
+                                                <Button
+                                                    loading={
+                                                        deleteDiagnosaId ===
+                                                        item.ID
+                                                    }
+                                                    className="btn btn-xs btn-outline btn-error"
+                                                    onClick={() =>
+                                                        showDeleteConfirm(item)
+                                                    }
+                                                >
+                                                    hapus
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
