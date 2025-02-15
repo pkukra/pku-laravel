b9007b4cabcc8148aeb1ceddebf55c343831f876
iqbal <emixbal@gmail.com>
2025-02-15 14:05:39 +0700

    init brdige component

--- resources/js/Pages/RM/PasienRujukan/PasienRujukanDetailBridging.jsx
+++ resources/js/Pages/RM/PasienRujukan/PasienRujukanDetailBridging.jsx
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Spin, Card, Input, notification, Button } from "antd";
-import { EditOutlined } from "@ant-design/icons";
+import { Modal, Card, Button, Input, notification, Spin } from "antd";
 import axios from "axios";
-
-const { TextArea } = Input;
 
 export default function Index({ pasien }) {
     const [modalBridgeOpen, setModalBridgeOpen] = useState(false);
+
+    const handleBridgingData = () => {};
 
     useEffect(() => {}, []);
 
     return (
         <>
-            <Card>
-                <Button>Bridge Data</Button>
+            <Card title={"Sync Data ke Ekalim"}>
+                <Button onClick={() => setModalBridgeOpen(true)}>
+                    Bridge Data
+                </Button>
             </Card>
 
             <Modal
                 title="Bridging Data Ke INACBG"
                 open={modalBridgeOpen}
+                onCancel={() => setModalBridgeOpen(false)}
+                okText={"Ok, Kirim Data"}
+                onOk={() => handleBridgingData()}
             ></Modal>
         </>
     );
