c4400f310c62b45b77beed58d3f078fa13623988
iqbal <emixbal@gmail.com>
2025-04-14 12:01:50 +0700

    refactor audittrail

--- database/migrations/2025_03_28_042335_create_audit_trail_rm_actions_table.php
+++ database/migrations/2025_03_28_042335_create_audit_trail_rm_actions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('audit_trail_rm_actions', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 100)->unique();
             $table->timestamps();
         });
     }
--- database/seeders/AuditTrailRmActionsSeeder.php
+++ database/seeders/AuditTrailRmActionsSeeder.php
@@ -13,14 +13,13 @@ class AuditTrailRmActionsSeeder extends Seeder
     public function run(): void
     {
         $actions = [
-            ['name' => 'add diagnosis', 'created_at' => now()],
-            ['name' => 'delete diagnosis', 'created_at' => now()],
-            ['name' => 'add procedure', 'created_at' => now()],
-            ['name' => 'add procedure', 'created_at' => now()],
-            ['name' => 'delete procedure', 'created_at' => now()],
-            ['name' => 'change sep', 'created_at' => now()],
-            ['name' => 'bridging data inacbg', 'created_at' => now()],
-            ['name' => 'final data inacbg', 'created_at' => now()],
+            ['name' => 'add_diagnosis', 'created_at' => now()],
+            ['name' => 'delete_diagnosis', 'created_at' => now()],
+            ['name' => 'add_procedure', 'created_at' => now()],
+            ['name' => 'delete_procedure', 'created_at' => now()],
+            ['name' => 'change_sep', 'created_at' => now()],
+            ['name' => 'bridging_data_inacbg', 'created_at' => now()],
+            ['name' => 'final_data_inacbg', 'created_at' => now()],
         ];
 
         DB::table('audit_trail_rm_actions')->insert($actions);
