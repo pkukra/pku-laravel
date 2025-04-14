8c8df1478fac932620b65bbe0d1fd43e1bfd1298
iqbal <emixbal@gmail.com>
2025-04-14 13:27:10 +0700

    audit trail

--- app/Repositories/RM/PasienRujukanRepository.php
+++ app/Repositories/RM/PasienRujukanRepository.php
@@ -341,7 +341,7 @@ class PasienRujukanRepository
      * @param array $data
      * @return boolean
      */
-    public function 
+    public function
     saveDiagnosa($data)
     {
         $user = Auth::user();
@@ -570,15 +570,55 @@ class PasienRujukanRepository
      */
     public function deleteProcedureById($id)
     {
+        $user = Auth::user();
+        $conn = DB::connection('sqlsrvsimrs');
+
         try {
-            $deleted = DB::connection('sqlsrvsimrs')
+            // Mulai transaksi
+            $conn->beginTransaction();
+
+            $deletedProcedure = $conn
+                ->table('MR_TINDAKAN')
+                ->where('ID', $id)
+                ->first();
+
+            if (!$deletedProcedure) {
+                return false;
+            }
+
+            // Hapus data
+            $deleted = $conn
                 ->table('MR_TINDAKAN')
                 ->where('ID', $id)
                 ->delete();
 
-            return $deleted > 0;
+            if (!$deleted) {
+                $conn->rollBack();
+                return false;
+            }
+
+            // Catat audit trail
+            $auditSuccess = $this->auditTrail->insert([
+                "object_id"  => $deletedProcedure->MRTNOTRANSAKSI,
+                "action_id"  => 4,
+                "user_email" => $user->email,
+                "user_id"    => $user->id,
+                "created_at" => now()->timezone('Asia/Jakarta')->format('Y-m-d H:i:s'),
+                "data"       => $deletedProcedure,
+            ]);
+
+            if (!$auditSuccess) {
+                Log::error("PasienRujukanRepository deleteProcedureById error: gagal simpan audittrail");
+                $conn->rollBack();
+                return false;
+            }
+
+            // Jika semuanya sukses
+            $conn->commit();
+            return true;
         } catch (\Exception $e) {
-            // Handle exception (logging, etc.)
+            $conn->rollBack();
+            Log::error("PasienRujukanRepository deleteProcedureById error: " . $e->getMessage());
             return false;
         }
     }
