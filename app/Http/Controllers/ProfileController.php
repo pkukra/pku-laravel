a5a0effbaf35aab0256e70e2ac7b0d06a77318f6
iqbal <emixbal@gmail.com>
2025-04-28 12:06:11 +0700

    show access role

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,6 +19,7 @@ class ProfileController extends Controller
     public function edit(Request $request): Response
     {
         return Inertia::render('Profile/Edit', [
+            'role' => Auth::user()->role,
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
         ]);
