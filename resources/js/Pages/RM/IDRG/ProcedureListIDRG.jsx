35d0aa157b995c0c215dfcb048b24bb0c2e7c4ea
iqbal <emixbal@gmail.com>
2025-06-12 08:49:35 +0700

    fix double width

--- resources/js/Pages/RM/IDRG/ProcedureListIDRG.jsx
+++ resources/js/Pages/RM/IDRG/ProcedureListIDRG.jsx
@@ -34,7 +34,6 @@ export default function Index({ pasien, isFinalIDRG, fetchIDRGData }) {
                     </>
                 );
             },
-            width: 100,
         },
         {
             title: "Penyakit",
