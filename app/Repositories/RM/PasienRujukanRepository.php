60d1bea23066fc1fca418424180e273cc92f9ad5
iqbal <emixbal@gmail.com>
2025-06-17 12:32:18 +0700

    walaupun belum punya emr akan muncul dulu

--- app/Repositories/RM/PasienRujukanRepository.php
+++ app/Repositories/RM/PasienRujukanRepository.php
@@ -61,20 +61,20 @@ class PasienRujukanRepository
     {
         $baseQuery = DB::connection('sqlsrvsimrs')
             ->table('PASIEN_RUJUKAN')
-            ->when($kode_poly != 'PK029', function ($query) {
-                $query->where(function ($query) {
-                    $query->whereExists(function ($sub) {
-                        $sub->select(DB::raw(1))
-                            ->from('PKU.dbo.TAC_RJ_MEDIS')
-                            ->whereColumn('PKU.dbo.TAC_RJ_MEDIS.FS_KD_REG', 'PASIEN_RUJUKAN.FRPNOTRANSAKSI');
-                    })
-                        ->orWhereExists(function ($sub) {
-                            $sub->select(DB::raw(1))
-                                ->from('PKU.dbo.TAC_IGD_MEDIS')
-                                ->whereColumn('PKU.dbo.TAC_IGD_MEDIS.KD_REG', 'PASIEN_RUJUKAN.FRPNOTRANSAKSI');
-                        });
-                });
-            })
+            // ->when($kode_poly != 'PK029', function ($query) {
+            //     $query->where(function ($query) {
+            //         $query->whereExists(function ($sub) {
+            //             $sub->select(DB::raw(1))
+            //                 ->from('PKU.dbo.TAC_RJ_MEDIS')
+            //                 ->whereColumn('PKU.dbo.TAC_RJ_MEDIS.FS_KD_REG', 'PASIEN_RUJUKAN.FRPNOTRANSAKSI');
+            //         })
+            //             ->orWhereExists(function ($sub) {
+            //                 $sub->select(DB::raw(1))
+            //                     ->from('PKU.dbo.TAC_IGD_MEDIS')
+            //                     ->whereColumn('PKU.dbo.TAC_IGD_MEDIS.KD_REG', 'PASIEN_RUJUKAN.FRPNOTRANSAKSI');
+            //             });
+            //     });
+            // })
             ->leftJoin('PASIEN', 'PASIEN_RUJUKAN.FRPPASIEN_ID', '=', 'PASIEN.KD_PASIEN')
             ->leftJoin('DOKTER', 'PASIEN_RUJUKAN.FRPDOKTER_ID', '=', 'DOKTER.FMDDOKTER_ID')
             ->leftJoin('POLIKLINIK', 'PASIEN_RUJUKAN.FRPUNIT', '=', 'POLIKLINIK.FMPKLINIK_ID')
