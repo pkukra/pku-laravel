35537789110f54f0fbffaa3a80858ce75115adf0
iqbal <emixbal@gmail.com>
2026-06-26 14:43:10 +0700

    ganti urutan

--- resources/js/Pages/Klaim/Inap/KumpulanButtonKlaimInapOnList.jsx
+++ resources/js/Pages/Klaim/Inap/KumpulanButtonKlaimInapOnList.jsx
@@ -20,23 +20,23 @@ export default function Index({ no_sep, nomer_rm, kode_reg }) {
                 <Col span={6}>
                     <SEP kode_reg={kode_reg} />
                     <Resume kode_reg={kode_reg} />
-                </Col>
-
-                <Col span={6}>
                     <SPRI kode_reg={kode_reg} nomer_rm={nomer_rm} />
-                    <Triase kode_reg={kode_reg} nomer_rm={nomer_rm}/>
-                </Col>
-
-                <Col span={6}>
-                    <LabRadiologi kode_reg={kode_reg} nomer_rm={nomer_rm} />
-                    <Kwitansi kode_reg={kode_reg} />
+                    <Triase kode_reg={kode_reg} nomer_rm={nomer_rm} />
                 </Col>
 
                 <Col span={6}>
                     <LaporanOperasi kode_reg={kode_reg} />
                     <Anastesi kode_reg={kode_reg} />
                     <LaporanVK kode_reg={kode_reg} />
+                </Col>
+
+                <Col span={6}>
+                    <LabRadiologi kode_reg={kode_reg} nomer_rm={nomer_rm} />
                     <PenunjangLain kode_reg={kode_reg} />
+                </Col>
+
+                <Col span={6}>
+                    <Kwitansi kode_reg={kode_reg} />
                     <EKlaim no_sep={no_sep} />
                 </Col>
             </Row>
