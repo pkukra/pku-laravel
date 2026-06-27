ee5194210fca214c13f0d300e13a17fe876af8ba
iqbal <emixbal@gmail.com>
2026-06-27 15:08:50 +0700

    ok

---
+++ resources/js/Pages/Klaim/Inap/Farmasi.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button, Modal, Skeleton } from "antd";
+
+export default function Index({ kode_reg }) {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [loadingPdf, setLoadingPdf] = useState(true); // Tambahkan state loading
+
+    return (
+        <>
+            <Button block size="small" onClick={() => setModalOpen(true)} style={{ margin: "2px" }}>
+                Farmasi
+            </Button>
+
+            {/* Modal  */}
+            <Modal
+                title="Preview Faktur Farmasi"
+                open={modalOpen}
+                onCancel={() => setModalOpen(false)}
+                footer={null}
+                width={800}
+            >
+                {/* Loading Indicator */}
+                {loadingPdf && (
+                    <>
+                        <Skeleton active />
+                    </>
+                )}
+
+                {/* PDF Viewer */}
+                <iframe
+                    src={route("klaim.inap.faktur_farmasi", { kode_reg })}
+                    width="100%"
+                    height="600px"
+                    style={{
+                        border: "none",
+                        display: loadingPdf ? "none" : "block",
+                    }}
+                    onLoad={() => setLoadingPdf(false)}
+                ></iframe>
+            </Modal>
+        </>
+    );
+}
