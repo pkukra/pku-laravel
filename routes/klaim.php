43c30c1fb9492f2a116d59c6b0738486d1f9d8cc
emixbal <emixbal@gmail.com>
2026-06-27 09:23:31 +0700

    ok

--- routes/klaim.php
+++ routes/klaim.php
@@ -24,11 +24,32 @@ Route::prefix('klaim')->group(function () {
         Route::get('/sep/{kode_reg}', [SEPController::class, 'index'])->name('klaim.inap.sep');
         Route::get('/laporan_anastesi/{kode_reg}', [LaporanAnastesiController::class, 'generatePdf'])->name('klaim.inap.laporan_anastesi');
         Route::get('/laporan_anastesi_snapshot/{kode_reg}', [LaporanAnastesiController::class, 'snapshot'])->name('klaim.inap.laporan_anastesi_snapshot');
+        Route::get('/laporan_ok_snapshot/{kode_reg}', [LaporanOKController::class, 'snapshot'])->name('klaim.inap.laporan_ok_snapshot');
 
         Route::get('/penunjang_lain/{kode_reg}', [PenunjangLainController::class, 'list'])->name('klaim.inap.penunjang_lain.list');
         Route::post('/penunjang_lain/{kode_reg}', [PenunjangLainController::class, 'upload'])->name('klaim.inap.penunjang_lain.upload');
         Route::get('/penunjang_lain/{kode_reg}/{id}', [PenunjangLainController::class, 'download'])->name('klaim.inap.penunjang_lain.download');
         Route::delete('/penunjang_lain/{kode_reg}/{id}', [PenunjangLainController::class, 'delete'])->name('klaim.inap.penunjang_lain.delete');
         Route::get('/cetak_klaim/{no_sep}', [PasienInapController::class, 'bridging_cetak_klaim'])->name('klaim.inap.cetak_klaim');
+
+        // Route::get('/test-browser', function () {
+
+        //     Spatie\Browsershot\Browsershot::url('https://google.com')
+        //         ->setNodeBinary('/usr/bin/node')
+        //         ->setNpmBinary('/usr/bin/npm')
+        //         ->setChromePath('/usr/bin/google-chrome')
+        //         ->noSandbox()
+        //         ->save(storage_path('app/test.png'));
+
+        //     return 'OK';
+        // });
+
+        // Route::get('/test-pdfcpu', function () {
+        //     return [
+        //         'exists' => file_exists('/usr/local/bin/pdfcpu'),
+        //         'is_executable' => is_executable('/usr/local/bin/pdfcpu'),
+        //         'realpath' => realpath('/usr/local/bin/pdfcpu'),
+        //     ];
+        // });
     });
 });
