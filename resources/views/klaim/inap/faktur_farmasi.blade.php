0be69bbc120e403692075bf3dc8b6beeadb1aea8
iqbal <emixbal@gmail.com>
2026-06-29 13:49:55 +0700

    ok

--- resources/views/klaim/inap/faktur_farmasi.blade.php
+++ resources/views/klaim/inap/faktur_farmasi.blade.php
@@ -89,60 +89,6 @@
     <div class="title-rs"> RS. PKU MUHAMMADIYAH KARANGANYAR </div>
     <div class="title"> PERINCIAN BIAYA FARMASI </div>
 
-    <table width="100%" style="margin-bottom:10px;">
-        <tr>
-            <td width="55%" valign="top">
-
-                <table width="100%">
-                    <tr>
-                        <td width="120">No. Med. Rec</td>
-                        <td width="10">:</td>
-                        <td>{{ $FMPASIEN_ID }}</td>
-                    </tr>
-                    <tr>
-                        <td>Nama Pasien</td>
-                        <td>:</td>
-                        <td>{{ $FMNAMA_PESERTA }}</td>
-                    </tr>
-                    <tr>
-                        <td>Alamat</td>
-                        <td>:</td>
-                        <td>{{ $ALAMAT }}</td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal Lahir</td>
-                        <td>:</td>
-                        <td>{{ \Carbon\Carbon::parse($TGL_LAHIR)->translatedFormat('d F Y') }}</td>
-                    </tr>
-                </table>
-            </td>
-            <td width="45%" valign="top">
-                <table width="100%">
-                    <tr>
-                        <td width="120">No. Registrasi</td>
-                        <td width="10">:</td>
-                        <td>{{ $FMNOTRANSAKSI }}</td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal Masuk</td>
-                        <td>:</td>
-                        <td>{{ \Carbon\Carbon::parse($PRWITGL_MASUK)->translatedFormat('d F Y') }}</td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal Pulang</td>
-                        <td>:</td>
-                        <td>{{ \Carbon\Carbon::parse($PRWITGL_KELUAR)->translatedFormat('d F Y') }}</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-
     <table class="table-obat">
         <thead>
             <tr>
@@ -158,43 +104,238 @@
                 <th width="12%">Total</th>
             </tr>
         </thead>
-        <tbody> @php $grandTotal = 0; $grandAdm = 0; @endphp @foreach($data as $resep) @php $subtotal = 0; $admTotal = 0; @endphp <tr class="group-header">
-                <td> {{ $resep->FHFJBUKTI_ID }} </td>
-                <td> {{ \Carbon\Carbon::parse($resep->FHFJDATE)->format('d-m-y') }} </td>
-                <td colspan="8"> {{ $resep->FHFJDOKTERN }} </td>
-            </tr> @foreach($resep->items as $item) @php $harga = (float) $item->FDFJHJUAL; $qty = (float) $item->FDFJQTY; $disc = (float) $item->FDFJDISC1; $discRp = (float) $item->FDFJDISC4; $adm = (float) $item->FDFJEMBALAGE; $total = (float) $item->FDFJTOTAL; $subtotal += $total; $admTotal += $adm; @endphp <tr>
-                <td class="center"></td>
+
+        <tbody>
+
+            @php
+            $grandTotal = 0;
+            $grandRetur = 0;
+            $grandRacik = 0;
+            $grandResep = 0;
+            $grandBulat = 0;
+            @endphp
+
+            @foreach($data as $resep)
+
+            {{-- ===================== RETUR ===================== --}}
+            @if($resep->FDTJENISTRANSAKSI == 'KR')
+
+            @php
+            $grandRetur += (float)$resep->FDTKREDIT;
+            @endphp
+
+            <tr class="group-header">
+                <td colspan="3">
+                    {{ $resep->FDTNO_FAKTUR }}
+                </td>
+                <td colspan="7">
+                    <b>RETUR OBAT</b>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="9" class="right">
+                    Pengurangan karena retur obat
+                </td>
+                <td class="right">
+                    ({{ number_format($resep->FDTKREDIT,0,',','.') }})
+                </td>
+            </tr>
+
+            @continue
+
+            @endif
+
+
+            {{-- ===================== RESEP ===================== --}}
+
+            @php
+            $subtotal = 0;
+            @endphp
+
+            <tr class="group-header">
+                <td colspan="3">
+                    {{ $resep->FDTNO_FAKTUR }}
+                    -
+                    {{ trim($resep->FHFJDOKTERN) }}
+                    -
+                    {{ \Carbon\Carbon::parse($resep->FDTTGL_TRANSAKSI)->translatedFormat('d/m/Y') }}
+                </td>
+
+                <td colspan="7"></td>
+            </tr>
+
+            @foreach($resep->items as $item)
+
+            @php
+            $harga = (float)$item->FDFJHJUAL;
+            $qty = (float)$item->FDFJQTY;
+            $disc = (float)$item->FDFJDISC1;
+            $discRp = (float)$item->FDFJDISC4;
+            $total = (float)$item->FDFJTOTAL;
+
+            $subtotal += $total;
+            @endphp
+
+            <tr>
                 <td></td>
-                <td> {{ trim($item->FDFJBRGN) }} </td>
-                <td class="center"> {{ trim($item->FDFJSATUAN) }} </td>
-                <td class="right"> {{ number_format($harga,0,',','.') }} </td>
-                <td class="center"> {{ rtrim(rtrim($qty,'0'),'.') }} </td>
-                <td class="center"> {{ $disc }} </td>
-                <td class="right"> {{ number_format($discRp,0,',','.') }} </td>
-                <td class="right"> {{ number_format($adm,0,',','.') }} </td>
-                <td class="right"> {{ number_format($total,0,',','.') }} </td>
-            </tr> @endforeach @php $grandTotal += $subtotal; $grandAdm += $admTotal; @endphp <tr class="subtotal">
-                <td colspan="8" class="right"> SUB TOTAL : </td>
-                <td class="right"> {{ number_format($admTotal,0,',','.') }} </td>
-                <td class="right"> {{ number_format($subtotal,0,',','.') }} </td>
-            </tr> @endforeach </tbody>
+                <td></td>
+                <td>{{ trim($item->FDFJBRGN) }}</td>
+                <td class="center">{{ trim($item->FDFJSATUAN) }}</td>
+                <td class="right">{{ number_format($harga,0,',','.') }}</td>
+                <td class="center">{{ rtrim(rtrim($qty,'0'),'.') }}</td>
+                <td class="center">{{ number_format($disc,0,',','.') }}</td>
+                <td class="right">{{ number_format($discRp,0,',','.') }}</td>
+                <td class="right">{{ number_format($item->FDFJEMBALAGE,0,',','.') }}</td>
+                <td class="right">{{ number_format($total,0,',','.') }}</td>
+            </tr>
+
+            @endforeach
+
+            @php
+
+            $racik = (float)$resep->FHFJRACIK;
+            $bulat = (float)$resep->FHFJBULAT;
+
+            $totalPerResep = $subtotal
+            + $racik
+            + $bulat;
+
+            $grandTotal += $totalPerResep;
+
+            $grandRacik += $racik;
+            $grandBulat += $bulat;
+
+            @endphp
+
+            <tr class="subtotal">
+                <td colspan="9" class="right">
+                    Sub Total Obat :
+                </td>
+                <td class="right">
+                    {{ number_format($subtotal,0,',','.') }}
+                </td>
+            </tr>
+
+            @if($racik > 0)
+            <tr>
+                <td colspan="9" class="right">
+                    Biaya Racik
+                </td>
+                <td class="right">
+                    {{ number_format($racik,0,',','.') }}
+                </td>
+            </tr>
+            @endif
+
+            @if($bulat != 0)
+            <tr>
+                <td colspan="9" class="right">
+                    Pembulatan
+                </td>
+                <td class="right">
+                    {{ number_format($bulat,0,',','.') }}
+                </td>
+            </tr>
+            @endif
+
+            <tr class="subtotal">
+                <td colspan="9" class="right">
+                    <b>TOTAL Per Transaksi :</b>
+                </td>
+                <td class="right">
+                    <b>{{ number_format($totalPerResep,0,',','.') }}</b>
+                </td>
+            </tr>
+
+            @endforeach
+
+        </tbody>
+
     </table>
+
+    @php
+    $totalAkhir = $grandTotal - $grandRetur;
+    @endphp
+
     <div class="grand-total">
+
         <table>
+
             <tr>
-                <td width="25%"> <b>DPHO :</b> </td>
-                <td width="25%"> 0 </td>
-                <td width="25%"> <b>NON DPHO :</b> </td>
-                <td width="25%" class="right"> {{ number_format($grandTotal,0,',','.') }} </td>
+                <td width="25%">
+                    <b>DPHO :</b>
+                </td>
+
+                <td width="25%">
+                    0
+                </td>
+
+                <td width="25%">
+                    <b>NON DPHO :</b>
+                </td>
+
+                <td width="25%" class="right">
+                    {{ number_format($grandTotal,0,',','.') }}
+                </td>
             </tr>
+
             <tr>
-                <td> <b>ADM :</b> </td>
-                <td> {{ number_format($grandAdm,0,',','.') }} </td>
-                <td> <b>TOTAL :</b> </td>
-                <td class="right"> <b>{{ number_format($grandTotal,0,',','.') }}</b> </td>
+
+                <td>
+                    <b>Biaya Racik :</b>
+                </td>
+
+                <td>
+                    {{ number_format($grandRacik,0,',','.') }}
+                </td>
+
+                <td>
+                    <b>Biaya Resep :</b>
+                </td>
+
+                <td class="right">
+                    {{ number_format($grandResep,0,',','.') }}
+                </td>
+
             </tr>
+
+            <tr>
+
+                <td>
+                    <b>Pembulatan :</b>
+                </td>
+
+                <td>
+                    {{ number_format($grandBulat,0,',','.') }}
+                </td>
+
+                <td>
+                    <b>RETUR :</b>
+                </td>
+
+                <td class="right">
+                    ({{ number_format($grandRetur,0,',','.') }})
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td colspan="3" class="right">
+                    <b>TOTAL :</b>
+                </td>
+
+                <td class="right">
+                    <b>{{ number_format($totalAkhir,0,',','.') }}</b>
+                </td>
+
+            </tr>
+
         </table>
+
     </div>
+
     <div class="footer"> PRINTED : {{ now()->format('d/M/Y h:i:s A') }} </div>
 </body>
 
